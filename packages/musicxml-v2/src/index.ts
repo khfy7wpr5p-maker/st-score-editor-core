a6c6749bf12c1d16aecc094dd38968e6327b5989
khfy7wpr5p-maker <onderozudogru@gmail.com>
2026-09-02 22:52:44 +0300

    SSE-06: export bounded MusicXML v2 API

---
+++ packages/musicxml-v2/src/index.ts
@@ -0,0 +1,5 @@
+export { parseMusicXmlV2Tree } from './parser.js';
+export type { ParsedMusicXmlV2Result } from './parser.js';
+export { serializeNotationMusicXmlV2 } from './serializer.js';
+export { importNotationMusicXmlV2 } from './importer.js';
+export type { NotationMusicXmlV2ImportResult } from './importer.js';
