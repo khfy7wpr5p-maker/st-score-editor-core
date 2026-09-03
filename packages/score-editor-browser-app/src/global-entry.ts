99279ed7000d3cdd65ee53f2138a8b0f6636259e
khfy7wpr5p-maker <onderozudogru@gmail.com>
2026-09-03 14:05:14 +0300

    APP-09 expose release hardened standalone runtime

--- packages/score-editor-browser-app/src/global-entry.ts
+++ packages/score-editor-browser-app/src/global-entry.ts
@@ -1,9 +1,9 @@
-import { createExportPrintEnabledStandaloneBrowserAppRuntime } from './export-print-enabled.js';
+import { createReleaseHardenedStandaloneBrowserAppRuntime } from './release-hardened.js';
 
 export const SCORE_EDITOR_APP_GLOBAL = 'STScoreEditorApp' as const;
 
 const target = globalThis as typeof globalThis & {
-  STScoreEditorApp?: ReturnType<typeof createExportPrintEnabledStandaloneBrowserAppRuntime>;
+  STScoreEditorApp?: ReturnType<typeof createReleaseHardenedStandaloneBrowserAppRuntime>;
 };
 
 if (Object.prototype.hasOwnProperty.call(target, SCORE_EDITOR_APP_GLOBAL)) {
@@ -11,7 +11,7 @@ if (Object.prototype.hasOwnProperty.call(target, SCORE_EDITOR_APP_GLOBAL)) {
 }
 
 Object.defineProperty(target, SCORE_EDITOR_APP_GLOBAL, {
-  value: createExportPrintEnabledStandaloneBrowserAppRuntime(),
+  value: createReleaseHardenedStandaloneBrowserAppRuntime(),
   writable: false,
   configurable: false,
   enumerable: true
