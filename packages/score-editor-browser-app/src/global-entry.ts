813f76a606e867bcc8ef6f3aa2f65f99511fec54
khfy7wpr5p-maker <onderozudogru@gmail.com>
2026-09-03 10:52:26 +0300

    APP-03A: expose standalone STScoreEditorApp browser global

---
+++ packages/score-editor-browser-app/src/global-entry.ts
@@ -0,0 +1,18 @@
+import { createStandaloneBrowserAppRuntime } from './index.js';
+
+export const SCORE_EDITOR_APP_GLOBAL = 'STScoreEditorApp' as const;
+
+const target = globalThis as typeof globalThis & {
+  STScoreEditorApp?: ReturnType<typeof createStandaloneBrowserAppRuntime>;
+};
+
+if (Object.prototype.hasOwnProperty.call(target, SCORE_EDITOR_APP_GLOBAL)) {
+  throw new Error('ST_SCORE_EDITOR_APP_ALREADY_DEFINED');
+}
+
+Object.defineProperty(target, SCORE_EDITOR_APP_GLOBAL, {
+  value: createStandaloneBrowserAppRuntime(),
+  writable: false,
+  configurable: false,
+  enumerable: true
+});
