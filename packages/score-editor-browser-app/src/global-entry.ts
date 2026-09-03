f35aaae4ac21c00d315d817498a434d27ab06815
khfy7wpr5p-maker <onderozudogru@gmail.com>
2026-09-03 11:04:45 +0300

    APP-04A: expose bounded local file adapter on standalone app global

--- packages/score-editor-browser-app/src/global-entry.ts
+++ packages/score-editor-browser-app/src/global-entry.ts
@@ -1,9 +1,31 @@
 import { createStandaloneBrowserAppRuntime } from './index.js';
+import {
+  browserFileWorkflowCapabilities,
+  readMusicXmlBrowserFile,
+  pickMusicXmlBrowserFile,
+  writeMusicXmlBrowserFile,
+  createMusicXmlDownloadArtifact,
+  MAX_LOCAL_MUSICXML_BYTES,
+  MUSICXML_MIME_TYPE
+} from '../../score-editor-browser-file-workflow/src/index.js';
 
 export const SCORE_EDITOR_APP_GLOBAL = 'STScoreEditorApp' as const;
 
+const createGlobalRuntime = () => Object.freeze({
+  ...createStandaloneBrowserAppRuntime(),
+  fileWorkflow: Object.freeze({
+    maxLocalMusicXmlBytes: MAX_LOCAL_MUSICXML_BYTES,
+    musicXmlMimeType: MUSICXML_MIME_TYPE,
+    capabilities: browserFileWorkflowCapabilities,
+    readFile: readMusicXmlBrowserFile,
+    pickFile: pickMusicXmlBrowserFile,
+    writeFile: writeMusicXmlBrowserFile,
+    createDownloadArtifact: createMusicXmlDownloadArtifact
+  })
+});
+
 const target = globalThis as typeof globalThis & {
-  STScoreEditorApp?: ReturnType<typeof createStandaloneBrowserAppRuntime>;
+  STScoreEditorApp?: ReturnType<typeof createGlobalRuntime>;
 };
 
 if (Object.prototype.hasOwnProperty.call(target, SCORE_EDITOR_APP_GLOBAL)) {
@@ -11,7 +33,7 @@ if (Object.prototype.hasOwnProperty.call(target, SCORE_EDITOR_APP_GLOBAL)) {
 }
 
 Object.defineProperty(target, SCORE_EDITOR_APP_GLOBAL, {
-  value: createStandaloneBrowserAppRuntime(),
+  value: createGlobalRuntime(),
   writable: false,
   configurable: false,
   enumerable: true
