31cfba8ca54f815aaaa6948b5c1fe39fddb5798e
khfy7wpr5p-maker <onderozudogru@gmail.com>
2026-09-04 20:36:38 +0300

    APP-10H make measure frame authoring the standalone runtime

--- packages/score-editor-browser-app/src/global-entry.ts
+++ packages/score-editor-browser-app/src/global-entry.ts
@@ -1,9 +1,9 @@
-import { createActiveStaffAuthoringStandaloneBrowserAppRuntime } from './active-staff-authoring.js';
+import { createMeasureFrameAuthoringStandaloneBrowserAppRuntime } from './measure-frame-authoring.js';
 
 export const SCORE_EDITOR_APP_GLOBAL = 'STScoreEditorApp' as const;
 
 const target = globalThis as typeof globalThis & {
-  STScoreEditorApp?: ReturnType<typeof createActiveStaffAuthoringStandaloneBrowserAppRuntime>;
+  STScoreEditorApp?: ReturnType<typeof createMeasureFrameAuthoringStandaloneBrowserAppRuntime>;
 };
 
 if (Object.prototype.hasOwnProperty.call(target, SCORE_EDITOR_APP_GLOBAL)) {
@@ -11,7 +11,7 @@ if (Object.prototype.hasOwnProperty.call(target, SCORE_EDITOR_APP_GLOBAL)) {
 }
 
 Object.defineProperty(target, SCORE_EDITOR_APP_GLOBAL, {
-  value: createActiveStaffAuthoringStandaloneBrowserAppRuntime(),
+  value: createMeasureFrameAuthoringStandaloneBrowserAppRuntime(),
   writable: false,
   configurable: false,
   enumerable: true
