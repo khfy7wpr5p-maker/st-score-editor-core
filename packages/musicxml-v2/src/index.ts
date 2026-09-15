2be7e532679b4661e46ec95692cd9f7d9f2e94bd
khfy7wpr5p-maker <onderozudogru@gmail.com>
2026-09-15 17:05:46 +0300

    S06E keep legacy serializer out of identity bridge

--- packages/musicxml-v2/src/index.ts
+++ packages/musicxml-v2/src/index.ts
@@ -5,14 +5,10 @@ export {
   importNotationMusicXmlV2 as importNotationMusicXmlV2Legacy
 } from './importer.js';
 export type { NotationMusicXmlV2ImportResult } from './importer.js';
-export {
-  serializeNotationMusicXmlV2 as serializeNotationMusicXmlV2Legacy
-} from './serializer.js';
+export { serializeNotationMusicXmlV2 } from './serializer.js';
 
 export {
   MUSICXML_NOTE_IDENTITY_BRIDGE_VERSION,
   importNotationMusicXmlV2PreservingNoteIds,
-  serializeNotationMusicXmlV2PreservingNoteIds,
-  importNotationMusicXmlV2PreservingNoteIds as importNotationMusicXmlV2,
-  serializeNotationMusicXmlV2PreservingNoteIds as serializeNotationMusicXmlV2
+  importNotationMusicXmlV2PreservingNoteIds as importNotationMusicXmlV2
 } from './identity.js';
