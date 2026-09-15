228fd6ad395d600c3f110e23f9593981e6e565d9
khfy7wpr5p-maker <onderozudogru@gmail.com>
2026-09-15 17:00:16 +0300

    S06E expose identity-preserving canonical v2 MusicXML API

--- packages/musicxml-v2/src/index.ts
+++ packages/musicxml-v2/src/index.ts
@@ -1,5 +1,18 @@
 export { parseMusicXmlV2Tree } from './parser.js';
 export type { ParsedMusicXmlV2Result } from './parser.js';
-export { serializeNotationMusicXmlV2 } from './serializer.js';
-export { importNotationMusicXmlV2 } from './importer.js';
+
+export {
+  importNotationMusicXmlV2 as importNotationMusicXmlV2Legacy
+} from './importer.js';
 export type { NotationMusicXmlV2ImportResult } from './importer.js';
+export {
+  serializeNotationMusicXmlV2 as serializeNotationMusicXmlV2Legacy
+} from './serializer.js';
+
+export {
+  MUSICXML_NOTE_IDENTITY_BRIDGE_VERSION,
+  importNotationMusicXmlV2PreservingNoteIds,
+  serializeNotationMusicXmlV2PreservingNoteIds,
+  importNotationMusicXmlV2PreservingNoteIds as importNotationMusicXmlV2,
+  serializeNotationMusicXmlV2PreservingNoteIds as serializeNotationMusicXmlV2
+} from './identity.js';
