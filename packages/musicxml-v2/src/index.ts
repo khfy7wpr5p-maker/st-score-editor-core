6e2e8c7820c220e0d6ac157bae83701accd9b73e
khfy7wpr5p-maker <onderozudogru@gmail.com>
2026-09-15 17:11:09 +0300

    S06E restore canonical MusicXML v2 exports

--- packages/musicxml-v2/src/index.ts
+++ packages/musicxml-v2/src/index.ts
@@ -1,14 +1,5 @@
 export { parseMusicXmlV2Tree } from './parser.js';
 export type { ParsedMusicXmlV2Result } from './parser.js';
-
-export {
-  importNotationMusicXmlV2 as importNotationMusicXmlV2Legacy
-} from './importer.js';
-export type { NotationMusicXmlV2ImportResult } from './importer.js';
 export { serializeNotationMusicXmlV2 } from './serializer.js';
-
-export {
-  MUSICXML_NOTE_IDENTITY_BRIDGE_VERSION,
-  importNotationMusicXmlV2PreservingNoteIds,
-  importNotationMusicXmlV2PreservingNoteIds as importNotationMusicXmlV2
-} from './identity.js';
+export { importNotationMusicXmlV2 } from './importer.js';
+export type { NotationMusicXmlV2ImportResult } from './importer.js';
